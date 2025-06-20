e00b76c801cdebee1446c97876513b3f26a91b3f
Signeemmanuel <signeemmanuel28@gmail.com>
2025-06-20 15:50:42 +0100

    Prepare for Render deployment with Cloudinary and ONNX

--- detection/models.py
+++ detection/models.py
@@ -1,9 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model # More robust way to get the User model
+from cloudinary.models import CloudinaryField # Import CloudinaryField
 
+User = get_user_model()
 class Detection(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='detections')
-    image = models.ImageField(upload_to='detections/')
+    image = CloudinaryField('image') # 'image' is a common default for CloudinaryField
+    # image = models.ImageField(upload_to='detections/')    
     result = models.CharField(max_length=255)
     latitude = models.FloatField(null=True, blank=True)
     longitude = models.FloatField(null=True, blank=True)
@@ -14,3 +17,10 @@ class Detection(models.Model):
 
     def __str__(self):
         return f'{self.user.username} - {self.result}'
+    
+    class Meta:
+        # Optional: Add a verbose name for the admin interface or documentation
+        verbose_name = "Plant Detection"
+        verbose_name_plural = "Plant Detections"
+        # Optional: Order detections by creation time by default
+        ordering = ['-created_at']
